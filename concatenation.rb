1a7e171a7a5df9d01d4247aebcbae6586ae905b9
Sarita Raghunath <sarita.raghunath@gmail.com>
2018-08-06 19:11:51 +0000

    automatically backed up by learn

--- concatenation.rb
+++ concatenation.rb
@@ -1,3 +1,6 @@
 puts "What's your name?"
 name = gets.chomp 
 puts "my name is #{name} and you're watching disney channel"
+puts "What's your favorite channel"
+channel = gets.chomp 
+puts "my name is #{name} and #{channel}"
